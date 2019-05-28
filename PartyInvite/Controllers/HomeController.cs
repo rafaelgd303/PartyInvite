c38b29ed52227912e7c110d03086fb5ceadb3468
Rafal Pawlowski <rafal.pawlowski@outlook.com>
2019-05-28 16:28:54 +0200

    initial commit

---
+++ PartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvite.Models;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "A" : "B";
+            return View("MyView");
+        }
+    } 
+}
